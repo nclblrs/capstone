4caca81c2c078c7f362fbe37e63a062092aa99e2
jhake <jhake.aquino123@gmail.com>
2021-09-27 19:57:20 +0800

    add Dropdown component

---
+++ src/components/Dropdown/index.js
@@ -0,0 +1,108 @@
+import { useState, useEffect, useRef } from "react";
+import { usePopper } from "react-popper";
+import styled from "styled-components";
+
+const Dropdown = ({ popperComponent, children, closeOnClickInside = true }) => {
+  const [visible, setVisibility] = useState(false);
+
+  const referenceRef = useRef(null);
+  const popperRef = useRef(null);
+
+  const handleClickOutsidePopper = () => {
+    setVisibility(false);
+  };
+
+  const handleClickInsidePopper = (e) => {
+    e.stopPropagation();
+
+    if (closeOnClickInside) setVisibility(false);
+  };
+
+  const { styles, attributes } = usePopper(
+    referenceRef.current,
+    popperRef.current,
+    {
+      placement: "bottom-end",
+      modifiers: [
+        {
+          name: "offset",
+          enabled: true,
+        },
+      ],
+    }
+  );
+  useEffect(() => {
+    // listen for clicks and close dropdown on body
+    document.addEventListener("click", handleClickOutsidePopper);
+    return () => {
+      document.removeEventListener("click", handleClickOutsidePopper);
+    };
+  }, []);
+
+  function handleDropdownClick(e) {
+    e.stopPropagation();
+    setVisibility(!visible);
+  }
+
+  return (
+    <DropdownContainer ref={referenceRef} onClick={handleDropdownClick}>
+      {children}
+      <div
+        ref={popperRef}
+        style={styles.popper}
+        {...attributes.popper}
+        onClick={(e) => e.stopPropagation()}
+      >
+        <PopverContainer
+          style={styles.offset}
+          visible={visible}
+          onClick={handleClickInsidePopper}
+        >
+          {popperComponent}
+        </PopverContainer>
+      </div>
+    </DropdownContainer>
+  );
+};
+
+const DropdownContainer = styled.div``;
+
+const PopverContainer = styled.div`
+  display: ${(props) => (props.visible ? "flex" : "none")};
+  background-color: #0f2520;
+  border-radius: 4px;
+  box-shadow: 0 0 8px 0 rgba(0, 0, 0, 0.14);
+  padding: 5px;
+`;
+
+export const DropdownButtons = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  button,
+  a {
+    text-align: left;
+    display: flex;
+    align-items: center;
+    width: 100%;
+    height: 28px;
+    color: white;
+    background: none;
+    border: none;
+    margin: 0 1em;
+    cursor: pointer;
+    font-size: 18px;
+    text-decoration: none;
+    padding: 20px 20px;
+
+    &:hover {
+      background-color: #0f482f;
+      color: white;
+      border-radius: 5px;
+    }
+  }
+`;
+
+export default Dropdown;
